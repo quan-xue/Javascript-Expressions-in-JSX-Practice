d9e47a5a9adbb31e9c3054a66c0cd046252e5446
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 12:25:27 +0800

    Complete challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 //Create a react app from scratch.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Quan";
+const lName = "Xue";
+const date = new Date();
+const year = date.getFullYear();
+
 //It should display 2 paragraph HTML elements.
+
+ReactDOM.render(
+  <div>
+    <p>Created by {fName + " " + lName}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //The paragraphs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
